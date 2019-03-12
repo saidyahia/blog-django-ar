3c1f1eba8af6d082a745bcfb841d81f0bc87c73d
Ahmad Abouissa <abouissa.ahmad@gmail.com>
2019-03-12 22:59:14 +0300

    Add dummy data to display it in home page as posts

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'title': 'التدوينة الأولى',
+        'content': 'نص التدوينة الأولى كنص تجريبي',
+        'post_date': '15-3-2019',
+        'author': 'Ahmad Abouissa'
+    },
+    {
+        'title': 'التدوينة الثانية',
+        'content': 'نص التدوينة الثانية كنص تجريبي',
+        'post_date': '20-3-2019',
+        'author': 'Ali Khaled'
+    },
+    {
+        'title': 'التدوينة الثالثة',
+        'content': 'نص التدوينة الثالثة كنص تجريبي',
+        'post_date': '28-3-2019',
+        'author': 'Ahmad Abouissa'
+    },
+    {
+        'title': 'التدوينة الرابعة',
+        'content': 'نص التدوينة الرابعة كنص تجريبي',
+        'post_date': '15-3-2019',
+        'author': 'Mohamed Ali'
+    },
+]
+
 
 def home(request):
-    return render(request, 'blog/index.html', {'title': 'Home'})
+    context = {
+        'title': 'الصفحة الرئيسية',
+        'posts': posts
+    }
+    return render(request, 'blog/index.html', context)
