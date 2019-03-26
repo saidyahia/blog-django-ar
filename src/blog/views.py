41c20b22781c8a7d4079e05172e405a343606a66
Ahmad Abouissa <abouissa.ahmad@gmail.com>
2019-03-26 10:42:47 +0300

    Query to fetch posts and display it in index

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,37 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'title': 'التدوينة الأولى',
-        'content': 'نص التدوينة الأولى كنص تجريبي',
-        'post_date': '15-3-2019',
-        'author': 'Ahmad Abouissa'
-    },
-    {
-        'title': 'التدوينة الثانية',
-        'content': 'نص التدوينة الثانية كنص تجريبي',
-        'post_date': '20-3-2019',
-        'author': 'Ali Khaled'
-    },
-    {
-        'title': 'التدوينة الثالثة',
-        'content': 'نص التدوينة الثالثة كنص تجريبي',
-        'post_date': '28-3-2019',
-        'author': 'Ahmad Abouissa'
-    },
-    {
-        'title': 'التدوينة الرابعة',
-        'content': 'نص التدوينة الرابعة كنص تجريبي',
-        'post_date': '15-3-2019',
-        'author': 'Mohamed Ali'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
         'title': 'الصفحة الرئيسية',
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/index.html', context)
 
