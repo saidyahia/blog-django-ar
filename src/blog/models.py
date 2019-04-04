8b5d167258c470012772f2a399a96fb161664438
Ahmad Abouissa <abouissa.ahmad@gmail.com>
2019-04-05 00:14:57 +0300

    Add Comment Class

--- src/blog/models.py
+++ src/blog/models.py
@@ -15,3 +15,19 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-post_date', )
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return 'علق {} على {}.'.format(self.name, self.post)
+
+    class Meta:
+        ordering = ('-comment_date',)
